f5d47af36950b05f1c4f98bdf0d4c55dcc1191cd
jinwook song <wlsdnr129@naver.com>
2022-12-01 20:21:34 +0900

    style: button

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,11 @@ class App extends StatelessWidget {
         home: Scaffold(
       backgroundColor: const Color(0xFF181818),
       body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: horizontal2,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 40,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 80,
@@ -42,6 +43,52 @@ class App extends StatelessWidget {
                       ),
                     )
                   ],
+                )
+              ],
+            ),
+            const SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balence',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              '\$5 194 482',
+              style: TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.w600,
+                color: Colors.white,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: const Color(0xFFF2B33A),
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 50,
+                      vertical: 20,
+                    ),
+                    child: Text(
+                      'Transfer',
+                      style: TextStyle(
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
                 )
               ],
             )
