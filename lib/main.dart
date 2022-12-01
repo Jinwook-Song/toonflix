f66ce2ca521a76058d644d27a24f79022399422f
jinwook song <wlsdnr129@naver.com>
2022-12-01 22:39:01 +0900

    feat: widget component

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/widgets/Button.dart';
 
 void main() {
   runApp(App());
@@ -7,13 +8,12 @@ void main() {
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    double horizontal2 = 20;
     return MaterialApp(
         home: Scaffold(
       backgroundColor: const Color(0xFF181818),
       body: Padding(
         padding: const EdgeInsets.symmetric(
-          horizontal: 40,
+          horizontal: 20,
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -71,25 +71,18 @@ class App extends StatelessWidget {
               height: 30,
             ),
             Row(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: const Color(0xFFF2B33A),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: const Padding(
-                    padding: EdgeInsets.symmetric(
-                      horizontal: 50,
-                      vertical: 20,
-                    ),
-                    child: Text(
-                      'Transfer',
-                      style: TextStyle(
-                        fontSize: 20,
-                      ),
-                    ),
-                  ),
-                )
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Button(
+                  text: 'Transfer',
+                  bgColor: Color(0xFFF2B33A),
+                  textColor: Colors.black,
+                ),
+                Button(
+                  text: 'Request',
+                  bgColor: Color(0xFF1F2123),
+                  textColor: Colors.white,
+                ),
               ],
             )
           ],
