4b2cb185786495bc87378973705863e2d3d7ff99
jinwook song <wlsdnr129@naver.com>
2022-12-07 20:35:37 +0900

    style: card with transform

--- lib/main.dart
+++ lib/main.dart
@@ -113,12 +113,14 @@ class App extends StatelessWidget {
               height: 20,
             ),
             Container(
+              clipBehavior: Clip.hardEdge,
               decoration: BoxDecoration(
                   color: const Color(0xFF1F2123),
                   borderRadius: BorderRadius.circular(20)),
               child: Padding(
                 padding: const EdgeInsets.all(30),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
@@ -155,6 +157,17 @@ class App extends StatelessWidget {
                         )
                       ],
                     ),
+                    Transform.translate(
+                      offset: const Offset(-14, 40),
+                      child: Transform.scale(
+                        scale: 2,
+                        child: const Icon(
+                          Icons.euro_rounded,
+                          color: Colors.white,
+                          size: 96,
+                        ),
+                      ),
+                    )
                   ],
                 ),
               ),
