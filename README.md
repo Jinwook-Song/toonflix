b1721edc3f03f56aa038ec10b937dff6257832e9
jinwook song <wlsdnr129@naver.com>
2022-12-20 20:34:40 +0900

    feat: statefull widget

--- README.md
+++ README.md
@@ -109,3 +109,85 @@ class App extends StatelessWidget {
   }
 }
 ```
+
+---
+
+### Statefull widget
+
+setState를 통해 flutter에게 update 되었음을 알려줌
+
+```dart
+import 'package:flutter/material.dart';
+
+enum CalcMethod { plus, minus }
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void handleCounter(CalcMethod method) {
+    switch (method) {
+      case CalcMethod.plus:
+        setState(() {
+          counter++;
+        });
+        break;
+      case CalcMethod.minus:
+        // modify
+        counter--;
+        // notify to flutter
+        setState(() {});
+        break;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: const Color.fromARGB(255, 206, 188, 188),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Counter',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            '$counter',
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                iconSize: 40,
+                onPressed: () => handleCounter(CalcMethod.minus),
+                icon: const Icon(Icons.indeterminate_check_box),
+              ),
+              IconButton(
+                iconSize: 40,
+                onPressed: () => handleCounter(CalcMethod.plus),
+                icon: const Icon(Icons.add_box_rounded),
+              ),
+            ],
+          )
+        ],
+      )),
+    ));
+  }
+}
+```
--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,73 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/Button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
+
+enum CalcMethod { plus, minus }
 
 void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+
+  void handleCounter(CalcMethod method) {
+    switch (method) {
+      case CalcMethod.plus:
+        setState(() {
+          counter++;
+        });
+        break;
+      case CalcMethod.minus:
+        // modify
+        counter--;
+        // notify to flutter
+        setState(() {});
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-      backgroundColor: const Color(0xFF181818),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(
-            horizontal: 20,
-          ),
+      backgroundColor: const Color.fromARGB(255, 206, 188, 188),
+      body: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Counter',
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Text(
+            '$counter',
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 80,
+              IconButton(
+                iconSize: 40,
+                onPressed: () => handleCounter(CalcMethod.minus),
+                icon: const Icon(Icons.indeterminate_check_box),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      const Text(
-                        'Hey, Selena',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 28,
-                          fontWeight: FontWeight.w800,
-                        ),
-                      ),
-                      Text(
-                        'Welcome Back',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white.withOpacity(0.8),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 70,
-              ),
-              Text(
-                'Total Balence',
-                style: TextStyle(
-                  fontSize: 22,
-                  color: Colors.white.withOpacity(0.8),
-                ),
-              ),
-              const SizedBox(
-                height: 5,
-              ),
-              const Text(
-                '\$5 194 482',
-                style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.w600,
-                  color: Colors.white,
-                ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: const [
-                  Button(
-                    text: 'Transfer',
-                    bgColor: Color(0xFFF2B33A),
-                    textColor: Colors.black,
-                  ),
-                  Button(
-                    text: 'Request',
-                    bgColor: Color(0xFF1F2123),
-                    textColor: Colors.white,
-                  ),
-                ],
-              ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  const Text(
-                    'Wallets',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 36,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  Text(
-                    'view all',
-                    style: TextStyle(
-                      color: Colors.white.withOpacity(0.8),
-                      fontSize: 18,
-                    ),
-                  )
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              const CurrencyCard(
-                name: 'Euro',
-                code: 'EUR',
-                amount: '6 428',
-                icon: Icons.euro_rounded,
-                index: 0,
-              ),
-              const CurrencyCard(
-                name: 'Bitcoin',
-                code: 'BTC',
-                amount: '9 201',
-                icon: Icons.currency_bitcoin_rounded,
-                index: 1,
-              ),
-              const CurrencyCard(
-                name: 'Dollar',
-                code: 'USD',
-                amount: '6 302',
-                icon: Icons.money_off_rounded,
-                index: 2,
+              IconButton(
+                iconSize: 40,
+                onPressed: () => handleCounter(CalcMethod.plus),
+                icon: const Icon(Icons.add_box_rounded),
               ),
             ],
-          ),
-        ),
-      ),
+          )
+        ],
+      )),
     ));
   }
 }
